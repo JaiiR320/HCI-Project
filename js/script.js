bd15b2462cc1475b4ab6966c2c1b9583f08c486d
JaiiR320 <jair3202@gmail.com>
2022-12-01 17:27:57 -0500

    finished hi-fi prototype

--- js/script.js
+++ js/script.js
@@ -1,34 +1,56 @@
 function allowDrop(ev) {
-    console.log(ev.target);
-    if (ev.target.className == "taken slot" && ev.target.className != "slot") {
-        return;
-    }
-    console.log('allowDrop');
     ev.preventDefault();
 }
 
 function drag(ev) {
 	ev.dataTransfer.setData('text', ev.target.id);
-    ev.target.className = "card";
-    ev.target.parentNode.className = "slot";
-
+    emptyCheck();
 }
 
 function drop(ev) {
+    if (ev.target.className != "slot") {
+        return;
+    }
 	ev.preventDefault();
 	var data = ev.dataTransfer.getData('text');
 	ev.target.appendChild(document.getElementById(data));
-    ev.target.className = "taken slot";
+    emptyCheck();
+}
+
+function emptyCheck(){
+    var kitchen = document.getElementById('kitchen');
+    var server = document.getElementById('server');
+    var inventory = document.getElementById('inventory');
+    for (var i = 0; i < kitchen.children.length; i++) {
+        if (kitchen.children[i].children.length == 0) {
+            kitchen.children[i].className = "slot";
+        } else {
+            kitchen.children[i].className = "taken slot";
+        }
+    }
+    for (var i = 0; i < server.children.length; i++) {
+        if (server.children[i].children.length == 0) {
+            server.children[i].className = "slot";
+        } else {
+            server.children[i].className = "taken slot";
+        }
+    }
+    for (var i = 0; i < inventory.children.length; i++) {
+        if (inventory.children[i].children.length == 0) {
+            inventory.children[i].className = "slot";
+        } else {
+            inventory.children[i].className = "taken slot";
+        }
+    }
 }
 
 function addCard(){
     var name = document.getElementById('formname').value;
     var notes = document.getElementById('formnotes').value;
-    
+    console.log("attempting to add card");
     var inv = document.getElementById("inventory");
     for (var i = 0; i < inv.children.length; i++) {
-        if (inv.children[i].classList.length == 1) {
-            console.log(inv.children[i].id);
+        if (inv.children[i].className != "taken slot") {
             inv.children[i].className = "taken slot";
             var div = document.createElement("div");
             div.id = name;
@@ -45,8 +67,8 @@ function addCard(){
             div2.appendChild(p);
             div.appendChild(div2);
             inv.children[i].appendChild(div)
-
-            break;
+            console.log("added " + name + " to slot " + inv.children[i].id)
+            return;
         }
     }
 }
